c53a9b3eadeb7815357de7e287d4b63cd7e5db58
8i-ght <needboosts3545@gmail.com>
2024-02-13 13:05:22 -0600

    finish animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -62,6 +62,42 @@ import { Component } from '@angular/core';
         }))
       ]),
     ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 0.8
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        group([
+          animate(300, style({
+            'color': 'red'
+          })),
+          animate(800, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          }))
+        ])
+      ]),
+    ]),
   ]
 })
 export class AppComponent {
@@ -87,5 +123,13 @@ export class AppComponent {
     if (index !== -1) {
       this.list.splice(index, 1);
     }
+  }
+
+  animationStarted(event) {
+    console.log(event);
+  }
+
+  animationEnded(event) {
+    console.log(event)
   }
 }
